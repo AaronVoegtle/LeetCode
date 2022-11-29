80d969185cd80a249c63ad8fc361967fb81ae2bf
AaronVoegtle <49257028+AaronVoegtle@users.noreply.github.com>
2022-11-28 19:56:38 -0600

    Add files via upload
    initial upload

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            
+            Console.WriteLine(d);
+            
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"the range of integers is {min} to {max}");
+
+            int over = max + 3;
+            Console.WriteLine($"an example of overflow {over}");
+           
+            double minDec = double.MaxValue;
+            decimal maxDec = decimal.MaxValue;
+            Console.WriteLine($"decimal range {minDec}to {maxDec}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+
+            Console.WriteLine(c / d);
+
+            double e = 2.50 * Math.PI * Math.PI;
+
+            decimal f = 
+            decimal g = f ; 
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            */
+
+
+            int a = 5;
+            int b = 6;
+            if (a + b > 10)
+                Console.WriteLine("answer");
+
+
+
+
+            Console.Read();
+        }
+    }
+}
